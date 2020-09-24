f6807b787e1f4935bf7086872bdbd01c4b9cdb11
ram0502dgit <71358728+ram0502dgit@users.noreply.github.com>
2020-09-24 12:46:19 +1000

    Update entryPoint.sh
    # STOP Services WHEN Docker STOP is issued.

--- entryPoint.sh
+++ entryPoint.sh
@@ -9,45 +9,50 @@ echo "Script Start Time: $starttime >"
 
 # echo
 #
-ls -ltr /opt1/server_installs/apache_Tomcat/apache-tomcat-7.0.105/webapps/ROOT/
+#ls -ltr /opt1/server_installs/apache_Tomcat/apache-tomcat-7.0.105/webapps/ROOT/
+
+# Start httpd
+#
+echo -e "\n\n"
+echo "HTTPD Starting..."
+httpd -k start
+echo "HTTPD Started..."
+
+# Start tomcat
+#
+echo -e "\n\n"
+cd /opt1/server_installs/apache_Tomcat/apache-tomcat-7.0.105/bin/
+echo "Tomcat Starting..."
+./catalina.sh start
+echo "Tomcat Started..."
+
 
 # Stop Services
-
+#
 stop_services()
 {
 
-# Stop HTTPD
+# sleep before stopping Services
+#
 echo -e "\n\n\n"
+echo -e "\n STOP SERVICES"
+echo "Executing -SLEEP- before -STOP- Apache HTTPD STOP | Tomcat STOP..."
+echo "Sleeping..."
+sleep 7
+
+
+# Stop HTTPD
+echo -e "\n\n"
 echo "HTTPD Stopping..."
 httpd -k stop
 echo "HTTPD Stopped..."
 
 # Stop tomcat
 cd /opt1/server_installs/apache_Tomcat/apache-tomcat-7.0.105/bin/
-echo -e "\n\n\n"
+echo -e "\n\n"
 echo "Tomcat Stopping..."
 ./catalina.sh stop 7 -force
 echo "Tomcat Stopped..."
-
-}
-
-echo -e "\n\n\n"
-echo "Executing ---SLEEP--- before Apache HTTPD | Tomcat startup..."
-echo "Sleeping..."
-sleep 7
-
-# Start httpd
-echo -e "\n\n\n"
-echo "HTTPD Starting..."
-httpd -k start
-echo "HTTPD Started..."
-
-# Start tomcat
-echo -e "\n\n\n"
-cd /opt1/server_installs/apache_Tomcat/apache-tomcat-7.0.105/bin/
-echo "Tomcat Starting..."
-./catalina.sh start
-echo "Tomcat Started..."
 
 # Script End Time
 #
@@ -55,28 +60,32 @@ endtime="$(date)"
 echo -e "\n\n\n"
 echo "Script End Time: $endtime >"
 
-
-# END Script
+# Exit Container
 #
-echo -e "\n"
-echo "### END ###"
+echo -e "\n### Exiting Container - <EXIT 0> ###"
+exit 0
 
-# Keep Container Running >
+}
+
+# STOP Services WHEN Docker STOP is issued.
 #
-echo -e "\n\n\n"
-echo "Keep Container Running >"
-
+# trap -SIGSTOP- signal
+#
 trap 'stop_services' SIGKILL SIGTERM SIGSTOP
 
+# Keep Container Running - Wait for STOP Signal (docker -stop-) >
+#
+echo -e "\n\n\n"
+echo "Keep Container Running  - Wait for STOP Signal (docker -stop-) >"
+
+# Loop + Wait for Signal
+#
 while true
 do
 tail -100f /dev/null & wait $!
 done
 
-
-# Exit Container
 #
-#echo -e "\n Exiting Container..."
-#exit 0
-
+#
 # END
+#
